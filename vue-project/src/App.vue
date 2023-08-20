736ec8d798e62faf86df0087b756f2c3e1c42165
Raion1 <isak.holmqvist01@gmail.com>
2023-08-20 19:55:53 +0200

    Made the website responsive for desktop along with bettering the performance of the mobile version

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,43 +1,10 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import AddMovieForm from './components/AddMovieForm.vue'
 import Movies from './components/Movies.vue'
 </script>
 
 <template>
-  <header>
+    <header>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-
-
-    <div class="wrapper">
-        <Movies>
-        </Movies>
-
-
-    </div>
-  </header>
-
+        <Movies />
+    </header>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-}
-</style>
